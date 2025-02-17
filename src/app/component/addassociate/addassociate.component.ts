8c08877037e8c0473853e959f2bacaa6714bb8d0
gitcoder-23 <prabhatece21@gmail.com>
2025-02-17 21:11:56 +0530

    design done for popup

--- src/app/component/addassociate/addassociate.component.ts
+++ src/app/component/addassociate/addassociate.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-addassociate',
@@ -10,12 +11,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AddassociateComponent implements OnInit {
   title = 'Create Associate'
   isedit = false;
+  dialogdata: any;
+
 
   associateform!: FormGroup;  
 
-  constructor(private builder: FormBuilder) {}  
+  constructor(private builder: FormBuilder, private ref: MatDialogRef<AddassociateComponent>, @Inject(MAT_DIALOG_DATA) public data: any,) {
+
+  }  
 
   ngOnInit(): void {
+    this.dialogdata = this.data;
+    this.title = this.dialogdata.title;
+
     // Initialize the form inside ngOnInit()
     this.associateform = this.builder.group({
       id: this.builder.control(0),
@@ -29,7 +37,9 @@ export class AddassociateComponent implements OnInit {
     });
   }
 
-  ClosePopup() {}
+  ClosePopup() {
+    this.ref.close()
+  }
 
   SaveAssociate() {}
 }
