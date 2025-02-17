199e8a266effafda1900dd3f051e294d4e8ba472
gitcoder-23 <prabhatece21@gmail.com>
2025-02-17 21:05:35 +0530

    working in the popup

--- src/app/component/addassociate/addassociate.component.ts
+++ src/app/component/addassociate/addassociate.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addassociate',
@@ -6,6 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './addassociate.component.html',
   styleUrl: './addassociate.component.css'
 })
-export class AddassociateComponent {
+export class AddassociateComponent implements OnInit {
+  title = 'Create Associate'
+  isedit = false;
 
+  associateform!: FormGroup;  
+
+  constructor(private builder: FormBuilder) {}  
+
+  ngOnInit(): void {
+    // Initialize the form inside ngOnInit()
+    this.associateform = this.builder.group({
+      id: this.builder.control(0),
+      name: this.builder.control('', Validators.required),
+      email: this.builder.control('', [Validators.required, Validators.email]),
+      phone: this.builder.control('', Validators.required),
+      address: this.builder.control('', Validators.required),
+      type: this.builder.control('CUSTOMER'),
+      group: this.builder.control('level1'),
+      status: this.builder.control(true)
+    });
+  }
+
+  ClosePopup() {}
+
+  SaveAssociate() {}
 }
--- src/app/component/associatelisting/associatelisting.component.ts
+++ src/app/component/associatelisting/associatelisting.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddassociateComponent } from '../addassociate/addassociate.component';
 
 @Component({
   selector: 'app-associatelisting',
@@ -6,6 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './associatelisting.component.html',
   styleUrl: './associatelisting.component.css'
 })
-export class AssociatelistingComponent {
+export class AssociatelistingComponent implements OnInit {
+  constructor(private dialog: MatDialog){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  FunctionAdd() {
+    this.OpenPopup(0, 'Create Associate');
+  }
 
+  OpenPopup(code: number, title: string){
+    this.dialog.open(AddassociateComponent, {
+      width: '50%',
+      enterAnimationDuration: '100ms',
+      exitAnimationDuration: '100ms',
+      data: {
+        code: code,
+        title: title,
+      }
+    });
+
+  }
 }
