3e2278ede874c7fccb5ca3be9420f7cfd8d35a48
gitcoder-23 <prabhatece21@gmail.com>
2025-02-17 21:25:53 +0530

    service creation done

--- src/app/service/associate.service.ts
+++ src/app/service/associate.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Associates } from '../Store/Model/Associate.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssociateService {
 
-  constructor() { }
+  baseUrl = "http://localhost:3305/associate";
+
+  constructor(private http: HttpClient) { }
+
+  GetAllAssociate() {
+    return this.http.get<Associates[]>(this.baseUrl);
+  }
+
+  GetAllAssociateByCode(code: number) {
+    return this.http.get<Associates>(`${this.baseUrl}/${code}`);
+  }
+
+
+  deleteAssociateByCode(code: number) {
+    return this.http.delete(`${this.baseUrl}/${code}`);
+  }
+
+
+
+
+  createAssociate(data:Associates) {
+    return this.http.put(this.baseUrl, data);
+  }
+
+
+  updateAssociate(data:Associates) {
+    return this.http.put(this.baseUrl+'/'+data.id, data);
+  }
 }
